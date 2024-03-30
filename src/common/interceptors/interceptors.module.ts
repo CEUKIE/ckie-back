ff39cde8c26b6d9fd3a561a48a7dae8786775199
Haesung <koh1260@naver.com>
2024-03-31 02:37:54 +0900

    feat: logging interceptor 구현

---
+++ src/common/interceptors/interceptors.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './logging.interceptor';
+
+const interceptors = [
+  { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+];
+
+@Module({
+  providers: [...interceptors],
+})
+export class InterceptorsModule {}
---
+++ src/common/interceptors/logging.interceptor.ts
@@ -0,0 +1,39 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Request, Response } from 'express';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Observable, tap } from 'rxjs';
+import { Logger } from 'winston';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER)
+    private readonly logger: Logger,
+    private readonly config: ConfigService,
+  ) {}
+
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<any> | Promise<Observable<any>> {
+    const ctx = context.switchToHttp();
+    const req = ctx.getRequest<Request>();
+    const { ip, path } = req;
+    const now = Date.now();
+
+    return next
+      .handle()
+      .pipe(
+        tap(() =>
+          this.logger.info(`\nip: ${ip}\npath: ${path}\n${Date.now() - now}ms`),
+        ),
+      );
+  }
+}
