8d2d75b8f09dea23a6b72c1b1274bfbe5c288fce
Haesung <koh1260@naver.com>
2024-04-02 13:36:58 +0900

    type: kakao 타입 수정

--- src/auth/types/index.ts
+++ src/auth/types/index.ts
@@ -20,17 +20,17 @@ export namespace Kakao {
     connected_at: 'string';
     properties: {
       nickname: string;
-      profile_image: string;
-      thumbnail_image: string;
+      profile_image?: string;
+      thumbnail_image?: string;
     };
     kakao_account: {
       profile_nickname_needs_agreement: boolean;
       profile_image_needs_agreement: boolean;
       profile: {
         nickname: string;
-        thumbnail_image_url: string;
-        profile_image_url: string;
-        is_default_image: boolean;
+        thumbnail_image_url?: string;
+        profile_image_url?: string;
+        is_default_image?: boolean;
         is_default_nickname: boolean;
       };
     };
