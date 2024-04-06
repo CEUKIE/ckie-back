f612daf0072f4fb43b702eafdca7755220185a1d
Haesung <koh1260@naver.com>
2024-04-07 05:29:04 +0900

    feat: 로그인 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { TokenData } from './types';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly config: ConfigService,
+  ) {}
+
+  async generateAccessToken(data: TokenData) {
+    return {
+      accessToken: await this.jwtService.signAsync(data),
+    };
+  }
+
+  async verifyAccessToken(token: string) {
+    return this.jwtService.verify(token, {
+      secret: this.config.get<string>('JWT_SECRET'),
+    });
+  }
+}
--- src/providers/users.service.ts
+++ src/providers/users.service.ts
@@ -1,14 +1,27 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { UserTypes } from '../types';
 import { UsersRepository } from '../repositories/users.repository';
+import { Platform } from '../auth/types';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class UsersService {
   constructor(
     @Inject(UsersRepository) private readonly usersRepository: UsersRepository,
+    private readonly authService: AuthService,
   ) {}
 
-  register(data: UserTypes.CreateUserData): Promise<string> {
-    return this.usersRepository.create(data);
+  async login(identifier: string, platform: Platform) {
+    return await this.authService.generateAccessToken({ identifier, platform });
+  }
+
+  async register(data: UserTypes.CreateUserData): Promise<string> {
+    return await this.usersRepository.create(data);
+  }
+
+  async getUserByIdentifier(
+    identifier: string,
+  ): Promise<UserTypes.UserDetail | null> {
+    return this.usersRepository.findOneByIdentifier(identifier);
   }
 }
