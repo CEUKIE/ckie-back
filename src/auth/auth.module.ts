6c841b091427baa6cc0114eaeacc3715fdd0b8d8
Haesung <koh1260@naver.com>
2024-04-07 04:37:45 +0900

    feat: jwt module 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { KakaoService } from './kakao.service';
+import { KakaoClient } from './kakao.client';
 import { AuthService } from './auth.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { UsersRepository } from '../repositories/users.repository';
+import { UsersPrismaRepository } from '../repositories/users.prisma.repository';
+import { UsersService } from '../providers/users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          global: true,
+          secret: config.get<string>('JWT_SECRET_KEY'),
+          signOptions: {
+            expiresIn: config.get<string>('JWT_TOKEN_EXPIRATION_TIME'),
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, KakaoService],
+  providers: [
+    AuthService,
+    KakaoClient,
+    UsersService,
+    { provide: UsersRepository, useClass: UsersPrismaRepository },
+  ],
 })
 export class AuthModule {}
