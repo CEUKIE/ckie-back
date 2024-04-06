be9ed3de029eda9a1d14369717795145fc9dc806
Haesung <koh1260@naver.com>
2024-04-07 08:48:38 +0900

    chore: 환경 변수 validation 항목 추가

--- src/configs/validationSchema.ts
+++ src/configs/validationSchema.ts
@@ -3,4 +3,9 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   KAKAO_LOGIN_API_KEY: Joi.string().required(),
   KAKAO_REDIRECT_URI: Joi.string().required(),
+  KAKAO_USER_DATA_URL: Joi.string().required(),
+  KAKAO_TOKEN_URL: Joi.string().required(),
+  DATABASE_URL: Joi.string().required(),
+  JWT_SECRET_KEY: Joi.string().required(),
+  JWT_TOKEN_EXPIRATION_TIME: Joi.string().required(),
 });
