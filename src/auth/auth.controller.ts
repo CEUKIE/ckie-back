0f3ef58bc52ae35cab42821faf2943f820ac2c06
Haesung <koh1260@naver.com>
2024-04-07 10:36:23 +0900

    chore: response form 적용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,16 +3,25 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { KakaoLoginDto } from './dto/kakao-login-dto';
 import { KakaoClient } from './oauth/kakao.client';
 import { LoginResponse } from './dto/login-response';
+import { ResponseForm } from '../common/format/response-form';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly kakaoService: KakaoClient) {}
 
+  /**
+   * @tag auth
+   * @summary 카카오 로그인
+   * @param dto 회원 정보
+   * @returns access token
+   */
   // TODO generate 오버로딩 함수 정의 후 endpoint 수정.
   @Post('kakao')
-  async kakaoLogin(@Body() dto: KakaoLoginDto): Promise<LoginResponse> {
+  async kakaoLogin(
+    @Body() dto: KakaoLoginDto,
+  ): Promise<ResponseForm<LoginResponse>> {
     const token: LoginResponse = await this.kakaoService.login(dto);
-    return token;
+    return ResponseForm.ok(token);
   }
 
   // 테스트 (삭제 예정)
