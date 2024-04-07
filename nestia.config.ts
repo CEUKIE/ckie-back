022e48b2bbaf6da34ab0c54bc05740b30a60d104
Haesung <koh1260@naver.com>
2024-04-07 09:43:43 +0900

    feat: nestia 설치

---
+++ nestia.config.ts
@@ -0,0 +1,35 @@
+import { INestiaConfig } from '@nestia/sdk';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './src/app.module';
+// import { FastifyAdaptor } from "@nestjs/platform-fastify";
+
+const NESTIA_CONFIG: INestiaConfig = {
+  input: async () => {
+    const app = await NestFactory.create(AppModule);
+    // const app = await NestFactory.create(YourModule, new FastifyAdaptor());
+    // app.setGlobalPrefix("api");
+    // app.enableVersioning({
+    //     type: VersioningType.URI,
+    //     prefix: "v",
+    // })
+    return app;
+  },
+  swagger: {
+    output: 'dist/swagger.json',
+    security: {
+      bearer: {
+        type: 'apiKey',
+        name: 'Authorization',
+        in: 'header',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:8080',
+        description: 'Local Server',
+      },
+    ],
+    beautify: true,
+  },
+};
+export default NESTIA_CONFIG;
