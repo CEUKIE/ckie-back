d047e435292986cabb123670e7f56d598834a4e3
Haesung <koh1260@naver.com>
2024-04-12 19:04:47 +0900

    chore: seed 추가

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,6 +9,7 @@ async function main() {
     where: { identifier: 'test' },
     update: {},
     create: {
+      id: 'cd8ea312-01d2-4821-8d2d-6a9950abac34',
       identifier: 'test',
       nickname: 'snail',
       introduction: 'introduction..',
@@ -16,7 +17,41 @@ async function main() {
     },
   });
 
-  console.log(user);
+  const species = await prisma.species.upsert({
+    where: { name: '달팽이' },
+    update: {},
+    create: {
+      name: '달팽이',
+      minTemperature: 1.2,
+      maxTemperature: 30.5,
+      minHumidity: 20.2,
+      maxHumidity: 60.7,
+      individuals: {
+        create: [
+          {
+            name: '퍙이',
+            weight: 3.2,
+            weightUnit: 'G',
+            memo: '응가쟁이임',
+            gender: 'FEMALE',
+            hatchedAt: '2024-04-12T09:50:17.029Z',
+            userId: 'cd8ea312-01d2-4821-8d2d-6a9950abac34',
+          },
+          {
+            name: '풍이',
+            weight: 56.2,
+            weightUnit: 'G',
+            memo: '똥쟁이임',
+            gender: 'FEMALE',
+            hatchedAt: '2024-04-12T09:50:17.029Z',
+            userId: 'cd8ea312-01d2-4821-8d2d-6a9950abac34',
+          },
+        ],
+      },
+    },
+  });
+
+  console.log([user, species]);
 }
 main()
   .then(async () => {
