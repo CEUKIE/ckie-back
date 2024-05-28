37bfddfff09ca4a014b6979a3ae217e447ac3cc3
Haesung <koh1260@naver.com>
2024-05-29 06:58:16 +0900

    refactor: 이미지 반환 url 도메인 적용

--- src/providers/files.service.ts
+++ src/providers/files.service.ts
@@ -1,10 +1,16 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ImageResponse } from '../types';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class FilesService {
+  constructor(private readonly config: ConfigService) {}
+
   upload(image: Express.MulterS3.File): ImageResponse {
     if (!image) throw new BadRequestException('이미지 없음');
-    return { filePath: image.location };
+    const filePath = `${this.config.get<string>('IMAGE_PREFIX')}${
+      image.location.split('amazonaws.com')[1]
+    }`;
+    return { filePath };
   }
 }
