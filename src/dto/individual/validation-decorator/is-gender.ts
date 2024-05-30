015cadf57a197c9b777b65aade49fa746ba4546f
Haesung <koh1260@naver.com>
2024-05-31 01:07:05 +0900

    refactor: 성별 검사에 less 추가

--- src/dto/individual/validation-decorator/is-gender.ts
+++ src/dto/individual/validation-decorator/is-gender.ts
@@ -7,7 +7,11 @@ import {
 @ValidatorConstraint({ name: 'isGender', async: false })
 export class IsGender implements ValidatorConstraintInterface {
   validate(value: string): boolean | Promise<boolean> {
-    return value.toUpperCase() === 'MALE' || value.toUpperCase() === 'FEMALE';
+    return (
+      value.toUpperCase() === 'MALE' ||
+      value.toUpperCase() === 'FEMALE' ||
+      value.toUpperCase() === 'LESS'
+    );
   }
 
   defaultMessage(args: ValidationArguments): string {
