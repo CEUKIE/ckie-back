1151ebd6a69afc578e9317ced8f43783385c0213
Haesung <koh1260@naver.com>
2024-05-31 11:19:30 +0900

    refactor: 개체 정보 수정 dto 재정의

--- src/dto/individual/update-individual.dto.ts
+++ src/dto/individual/update-individual.dto.ts
@@ -1,4 +1,50 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateIndividualDto } from './create-individual.dto';
+import {
+  IsDateString,
+  IsOptional,
+  IsString,
+  IsUUID,
+  Length,
+  Validate,
+} from 'class-validator';
 
-export class UpdateIndividualDto extends PartialType(CreateIndividualDto) {}
+import { IsGender } from './validation-decorator/is-gender';
+import { IndividualTypes } from '../../types';
+
+export class UpdateIndividualDto {
+  @IsString()
+  @Length(2, 10)
+  readonly name: string;
+
+  @IsString()
+  readonly avatarUrl: string;
+
+  @Validate(IsGender)
+  readonly gender: IndividualTypes.Gender;
+
+  @IsDateString()
+  readonly hatchedAt: Date;
+
+  @IsUUID()
+  readonly cageId: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(0, 50)
+  readonly memo?: string;
+
+  constructor(
+    name: string,
+    avatarUrl: string,
+    gender: IndividualTypes.Gender,
+    hatchedAt: Date,
+    cageId: string,
+    memo?: string,
+  ) {
+    this.name = name;
+    this.avatarUrl = avatarUrl;
+    this.gender = gender;
+    this.hatchedAt = hatchedAt;
+    this.cageId = cageId;
+    this.memo = memo;
+  }
+}
