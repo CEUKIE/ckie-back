bb8d19d8a71d41614d579f4d8f992913df692b50
Haesung <koh1260@naver.com>
2024-06-02 18:48:42 +0900

    chore: cors 해제

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
   await app.listen(8080);
 }
 bootstrap();
