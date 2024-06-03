c2c6b2bf065d30e86b5451ba5fb3573c82397627
Haesung <koh1260@naver.com>
2024-06-04 05:18:12 +0900

    fix: 날짜 포맷 수정

--- src/controllers/records.controller.ts
+++ src/controllers/records.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateRecordsDto } from '../dto/records/create-records-dto';
 import { RecordsService } from '../providers/records.service';
 import { ResponseForm } from '../common/format/response-form';
@@ -10,9 +18,10 @@ export class RecordsController {
   /**
    * @tag records
    * @summary 기록 생성
-   * @param dto category는 enum RecordCategory 타입 확인. memo: 0 ~ 15자
+   * @param dto category: enum RecordCategory 타입 확인, memo: 0 ~ 15자, weight(optional): 무게 기록일 때만 입력.
    * @returns 생성된 기록 데이터
    */
+  @HttpCode(200)
   @Post()
   async create(@Body() dto: CreateRecordsDto) {
     const respose = await this.recordsService.create(dto);
--- src/providers/records.service.ts
+++ src/providers/records.service.ts
@@ -75,8 +75,8 @@ export class RecordsService {
 
   foramtDate(date: Date) {
     const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
   }
 }
