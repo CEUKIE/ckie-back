22d0f3296897947aec6d623f24221dce68901963
Haesung <koh1260@naver.com>
2024-06-11 22:34:57 +0900

    feat: 전역 예외 필터 수정

--- src/common/filters/filter.module.ts
+++ src/common/filters/filter.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 
 import { HttpExceptionFilter } from './http-exception.filter';
+import { GlobalExceptionFilter } from './global-exception.filter';
 
-const filters = [{ provide: APP_FILTER, useClass: HttpExceptionFilter }];
+const filters = [{ provide: APP_FILTER, useClass: GlobalExceptionFilter }];
 
 @Module({
   providers: [...filters],
---
+++ src/common/filters/global-exception.filter.ts
@@ -0,0 +1,59 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  Inject,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+
+interface LoggingFormat {
+  level: 'warn' | 'error';
+  ip: string | undefined;
+  timestamp: Date;
+  path: string;
+  context: string;
+  message: string;
+  stack?: string;
+}
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
+
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const req: Request = ctx.getRequest();
+    const res: Response = ctx.getResponse();
+
+    const log: LoggingFormat = {
+      level: 'warn',
+      ip: req.ip,
+      timestamp: new Date(),
+      path: req.url,
+      context: exception.name,
+      message: exception.message,
+    };
+
+    const message = `\nlevel: ${log.level}\nip: ${log.ip}\npath: ${log.path}\ntimestamp: ${log.timestamp}\ncontext: ${log.context}\nmessage: ${log.message}\n`;
+
+    if (exception instanceof HttpException) {
+      this.logger.warn(message);
+    } else {
+      log.level = 'error';
+      log.stack = exception.stack;
+      this.logger.error(message);
+
+      exception = new InternalServerErrorException();
+    }
+
+    const response = (exception as HttpException).getResponse();
+
+    res.status((exception as HttpException).getStatus()).json(response);
+  }
+}
